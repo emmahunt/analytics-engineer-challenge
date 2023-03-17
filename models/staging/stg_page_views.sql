003e866d09c1a6382b9ef470a74a24bd49f57d45
Emma Hunt <emmaghunt95@gmail.com>
2023-03-17 18:23:13 +0000

    Add trimmed user id columns, and adjust description

--- models/staging/stg_page_views.sql
+++ models/staging/stg_page_views.sql
@@ -4,12 +4,15 @@ with raw_data as (
 )
 
 , final as (
-    
+
     -- Change column names and cases
     select
         NAME as page_path
         , RECEIVED_AT as received_at
         , USER_ID as user_id
+
+        -- Remove the characters at the end of the user_id that are repeated in all values
+        , substr(user_id, 10, 27) as user_id_trimmed
     from raw_data
 )
 
--- models/staging/stg_users.sql
+++ models/staging/stg_users.sql
@@ -4,11 +4,14 @@ with raw_data as (
 )
 
 , final as (
-    
+
     -- Change the case of the column names to lowercase
     -- Note that this is not strictly necessary in Snowflake, but other data warehouses (e.g. BigQuery) differentiate between uppercase and lowercase column names
     select
         USER_ID as user_id
+
+        -- Remove the characters at the start and end of the user id string that are repeated in all user ids
+        , substr(user_id, 6, 17 as user_id_trimmed
         , CREATED_AT as created_at
         , IS_INTERNAL as is_internal
         , SEGMENT as segment
