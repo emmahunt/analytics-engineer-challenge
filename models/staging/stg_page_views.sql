695619ba3ca27e03bd6c5e5048725a4460edcdc1
Emma Hunt <emmaghunt95@gmail.com>
2023-03-17 20:37:46 +0000

    Removed trimmed columns and adjustments to capitalisation of column selects

--- models/staging/stg_page_views.sql
+++ models/staging/stg_page_views.sql
@@ -3,16 +3,12 @@ with raw_data as (
     from {{ ref('pages_views') }}
 )
 
+-- Change column names
 , final as (
-
-    -- Change column names and cases
     select
-        NAME as page_path
-        , RECEIVED_AT as received_at
-        , USER_ID as user_id
-
-        -- Remove the characters at the end of the user_id that are repeated in all values
-        , substr(user_id, 10, 27) as user_id_trimmed
+        name as page_path
+        , received_at
+        , user_id
     from raw_data
 )
 
--- models/staging/stg_users.sql
+++ models/staging/stg_users.sql
@@ -4,17 +4,11 @@ with raw_data as (
 )
 
 , final as (
-
-    -- Change the case of the column names to lowercase
-    -- Note that this is not strictly necessary in Snowflake, but other data warehouses (e.g. BigQuery) differentiate between uppercase and lowercase column names
     select
-        USER_ID as user_id
-
-        -- Remove the characters at the start and end of the user id string that are repeated in all user ids
-        , substr(user_id, 6, 17 as user_id_trimmed
-        , CREATED_AT as created_at
-        , IS_INTERNAL as is_internal
-        , SEGMENT as segment
+        user_id
+        , created_at
+        , is_internal
+        , segment
     from raw_data
 )
 
