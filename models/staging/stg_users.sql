4f815b17bc236de440a56d63af6992b312fd2d6e
Emma Hunt <emmaghunt95@gmail.com>
2023-04-12 18:42:47 +0100

    Update materialisation to table to prevent random number generator from breaking referential integrity

--- models/staging/stg_users.sql
+++ models/staging/stg_users.sql
@@ -1,12 +1,15 @@
 with raw_data as (
-    select distinct user_id
+    select
+        user_identifier as user_id
+        , min(received_at) as created_at
     from {{ ref('stg_page_views') }}
+    group by 1
 )
 
 , generate_demographic_data as (
     select
         user_id
-        , min(received_at) over (partition by user_id order by received_at asc) as created_at
+        , created_at
         , uniform(13, 100, random()) as age
         , uniform(0, 250, random()) as country_id
     from raw_data
@@ -19,7 +22,7 @@ with raw_data as (
         , generate_demographic_data.age
         , stg_countries.country_name
     from generate_demographic_data
-    left join stg_countries
+    left join {{ ref('stg_countries') }}
         on generate_demographic_data.country_id = stg_countries.country_id
 )
 
